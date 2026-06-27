5acd3e6f1d5a06341db0842127c4913213f98ffa
dayanagarzon735 <dayanagarzon735@gmail.com>
2026-06-27 12:00:56 -0500

    Conectar frontend con backend

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,22 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://TU_BACKEND.onrender.com/api',
     headers: {
         'Content-Type': 'application/json'
     }
-});
-
-api.interceptors.request.use((config) => {
-
-    const token = localStorage.getItem('token');
-
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-
-    return config;
-
 });
 
 export default api;
